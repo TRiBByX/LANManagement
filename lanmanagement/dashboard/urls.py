4afeb42d2f40f7c2776c8ac5f3a4f71dbdc4aeb4
TRiBByX <christoffer.ssj@gmail.com>
2018-03-15 23:27:08 +0100

    Set up the urls for redirecting correctly

--- lanmanagement/dashboard/urls.py
+++ lanmanagement/dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
+from dashboard.views import Index
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', Index.as_view(), name='Dashboard'),
 ]
--- lanmanagement/dashboard/views.py
+++ lanmanagement/dashboard/views.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
 
 # Create your views here.
+
+class Index(View):
+    
+    def get(self, request):
+        return HttpResponse(200)
